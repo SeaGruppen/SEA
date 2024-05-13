a2b5bf11958e8e9359f048e0cb57b2a580cc3cc8
toppggg <70447358+toppggg@users.noreply.github.com>
2024-05-13 12:14:36 +0200

    Started implementing Survey. Breakpoint when group split up

--- src/Model/FrontEndAPI/FrontEndMainMenu.cs
+++ src/Model/FrontEndAPI/FrontEndMainMenu.cs
@@ -1,11 +1,12 @@
 namespace FrontEndAPI;
+
 using Survey;
 
     public static class FrontEndMainMenu {
         public static bool ValidateSuperUser(string username, string password) {
             return false;
         }
-        public static IGetSurvey? GetSurvey(int surveyId) {
+        public static IReadOnlySurvey? GetSurvey(int surveyId) {
             return null;
         }
     }
