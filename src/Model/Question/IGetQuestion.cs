5b4da079b605eaea5463e12f603697a915bee0ea
Nicolaj <nicolajRjensen@gmail.com>
2024-05-15 12:04:44 +0200

    Changes GetPicture to string

--- src/Model/Question/IGetQuestion.cs
+++ src/Model/Question/IGetQuestion.cs
@@ -2,10 +2,11 @@ namespace Question;
 
 using System.Drawing;
 
-public interface IGetQuestion {
-int GetId {get;}
-QuestionType GetType {get;}
-string GetCaption {get;}
-Image GetPicture {get;}
-string GetText {get;}
+public interface IGetQuestion
+{
+    int GetId { get; }
+    QuestionType GetType { get; }
+    string GetCaption { get; }
+    string GetPicture { get; }
+    string GetText { get; }
 }
