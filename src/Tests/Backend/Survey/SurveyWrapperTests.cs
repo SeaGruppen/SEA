16a3620a3bd10a9ac535f5e2aa5870ab0a3b9d31
toppggg <70447358+toppggg@users.noreply.github.com>
2024-06-03 13:01:56 +0200

    Updated namespace for consistency to Tests

---
+++ src/Tests/Backend/Survey/SurveyWrapperTests.cs
@@ -0,0 +1,25 @@
+﻿using Model.Survey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Backend.SurveyTests
+{
+    internal class SurveyWrapperTests
+    {
+        [Test]
+        public void TestAddNewVersion()
+        {
+            var sut = new Survey("1");
+
+            var res1 = sut.AddNewMultiQuestion();
+            var res2 = sut.AddNewMultiQuestion();
+
+            Assert.That(sut.SurveyQuestions.Count, Is.EqualTo(2));
+            Assert.That(res1.MultiQuestionId, Is.EqualTo("1.0"));
+            Assert.That(res2.MultiQuestionId, Is.EqualTo("1.1"));
+        }
+    }
+}
