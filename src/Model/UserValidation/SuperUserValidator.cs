61075b6819731625d7f4708b16ff02db4e7e50d0
Jacob Agertoft <jacob.agertoft@hotmail.com>
2024-06-09 16:35:43 +0200

    Save credentials in txt file

--- src/Model/UserValidation/SuperUserValidator.cs
+++ src/Model/UserValidation/SuperUserValidator.cs
@@ -1,5 +1,7 @@
 ﻿
 namespace Model.UserValidation;
+
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -7,18 +9,20 @@ internal class SuperUserValidator : ISuperUserValidator {
     // Created for testing purposes. Contains hashed passwords.
     public static IReadOnlyDictionary<string, string> SuperUserCredentials => superUserCredentials;
 
-    private static Dictionary<string, string> superUserCredentials = new Dictionary<string, string>();
+    private static Dictionary<string, string> superUserCredentials = ImportUserCredentials();
 
     public bool AddSuperUserCredentials(string username, string password) {
         // Check if user already exists
         if (!superUserCredentials.TryGetValue(username, out _)) {
             superUserCredentials[username] = HashPassword(password);
+            StoreUserCredentials();
             return true;
         }
         return false;
     }
 
     public void RemoveSuperUserCredentials(string username) {
+        StoreUserCredentials();
         superUserCredentials.Remove(username);
     }
 
@@ -30,6 +34,41 @@ internal class SuperUserValidator : ISuperUserValidator {
             return false;
 
         return true;
+    }
+
+    private void StoreUserCredentials()
+    {
+        File.WriteAllLines("Model\\UserCredentials\\UserCredentials.txt",
+            superUserCredentials.Select(x => "[" + x.Key + " " + x.Value + "]").ToArray());
+    }
+
+    private static Dictionary<string, string> ImportUserCredentials()
+    {
+        string filePath = "Model\\UserCredentials\\UserCredentials.txt";
+        Dictionary<string, string> userDictionary = new Dictionary<string, string>();
+
+        try
+        {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                string content = line.Trim('[', ']');
+
+                int spaceIndex = content.IndexOf(' ');
+                if (spaceIndex > 0)
+                {
+                    string username = content.Substring(0, spaceIndex);
+                    string password = content.Substring(spaceIndex + 1);
+
+                    userDictionary[username] = password;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+        return userDictionary;
     }
 
     private string HashPassword(string password) {
--- src/Tests/Backend/UserValidation/UserValidatorTests.cs
+++ src/Tests/Backend/UserValidation/UserValidatorTests.cs
@@ -1,4 +1,4 @@
-﻿using backend.UserValidation;
+﻿using Model.UserValidation;
 
 namespace Tests.Backend.UserValidation
 {
@@ -82,6 +82,44 @@ namespace Tests.Backend.UserValidation
             var res = sut.ValidateSuperUser("username", "wrong_password");
 
             Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void TestCreateFile()
+        {
+            var sut = new SuperUserValidator();
+
+            sut.AddSuperUserCredentials("username1", "password1");
+            sut.AddSuperUserCredentials("username2", "password2");
+            sut.AddSuperUserCredentials("username3", "password3");
+
+            File.WriteAllLines("..\\..\\..\\Backend\\UserCredentials\\myfile.txt",
+                SuperUserValidator.SuperUserCredentials.Select(x => "[" + x.Key + " " + x.Value + "]").ToArray());
+        }
+
+        [Test]
+        public void TestImportFile()
+        {
+            var pre = SuperUserValidator.SuperUserCredentials;
+
+            string filePath = "..\\..\\..\\Backend\\UserCredentials\\myfile.txt";
+            Dictionary<string, string> userDictionary = new Dictionary<string, string>();
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                string content = line.Trim('[', ']');
+
+                int spaceIndex = content.IndexOf(' ');
+                if (spaceIndex > 0)
+                {
+                    string username = content.Substring(0, spaceIndex);
+                    string password = content.Substring(spaceIndex + 1);
+
+                    userDictionary[username] = password;
+                }
+            }
+
+            var post = userDictionary;
         }
     }
 }
