0a59ef69b422156cf43c22a35eb7aa417003fd17
Jacob Agertoft <jacob.agertoft@hotmail.com>
2024-06-10 16:27:49 +0200

    Adhering to styleguide

--- src/Model/FrontEndAPI/FrontEndMainMenu.cs
+++ src/Model/FrontEndAPI/FrontEndMainMenu.cs
@@ -27,8 +27,7 @@ internal class FrontEndMainMenu : IFrontEndMainMenu {
 
     public List<IModifySurveyWrapper>? ValidateSuperUser(string username, string password) {
         //Validate superuser against Hashfunction first. If true, then return the list of surveys
-        if (superUserValidator.ValidateSuperUser(username, password))
-        {
+        if (superUserValidator.ValidateSuperUser(username, password)) {
             List<SurveyWrapper> surveyWrappers = db.GetSurveyWrapperForSuperUser(username);
             List<IModifySurveyWrapper> result = new List<IModifySurveyWrapper>(surveyWrappers.Cast<IModifySurveyWrapper>().ToList());
             return result;
