277f9c5d7eeb96a9cb84a751353db0c8a5b1f965
toppggg <70447358+toppggg@users.noreply.github.com>
2024-06-10 15:09:02 +0200

    Fixed check for key existing in DB

--- src/Model/Database/DatabaseServices.cs
+++ src/Model/Database/DatabaseServices.cs
@@ -260,6 +260,9 @@ internal class DatabaseServices : IDatabase {
 
     public List<SurveyWrapper> GetSurveyWrapperForSuperUser(string superUserName){
         Dictionary<string, List<int>> creatorDict = GetCreatorDict();
+        if (!creatorDict.ContainsKey(superUserName)) {
+            return new List<SurveyWrapper>();
+        }
         List<SurveyWrapper> surveyWrapperList = new List<SurveyWrapper>();
         foreach (int surveyWrapperId in creatorDict[superUserName]) {
             surveyWrapperList.Add(GetSurveyWrapper(surveyWrapperId));
