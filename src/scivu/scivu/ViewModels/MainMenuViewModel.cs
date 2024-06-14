9384c9be48f4821bf0369bb213807615dbc2e68c
toppggg <70447358+toppggg@users.noreply.github.com>
2024-06-14 21:32:12 +0200

    Import bug from MainMenu fixed, given by Nicolaj

--- src/scivu/scivu/ViewModels/MainMenuViewModel.cs
+++ src/scivu/scivu/ViewModels/MainMenuViewModel.cs
@@ -93,7 +93,7 @@ public class MainMenuViewModel : ViewModelBase
         var file = await FileExplorer.OpenSurveyAsync();
         if (file != null)
         {
-            var path = file.Path.ToString();
+            var path = file.Path.AbsolutePath.ToString();;
             if (_client.ImportSurveyWrapper(path))
             {
                 return;
